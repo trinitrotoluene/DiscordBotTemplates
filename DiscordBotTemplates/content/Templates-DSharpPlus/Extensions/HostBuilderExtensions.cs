dec17379bf2880a52c3f0f1a4ed29d2a44f37d9f
Trinitrotoluene <pmichallet00@gmail.com>
2019-06-11 15:39:25 +0200

    Add DSharpPlus template

---
+++ DiscordBotTemplates/content/Templates-DSharpPlus/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,41 @@
+﻿using DSharpPlus;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    internal static class HostBuilderExtensions
+    {
+        public static IHostBuilder ConfigureDiscord(this IHostBuilder builder, Action<DiscordConfiguration> configureOptions)
+        {
+            var config = new DiscordConfiguration();
+            configureOptions(config);
+
+            var client = new DiscordClient(config);
+
+            builder.ConfigureServices(services =>
+            {
+                services.AddSingleton(config);
+                services.AddSingleton(client);
+            });
+
+            return builder;
+        }
+
+        public static IHostBuilder ConfigureDiscord(this IHostBuilder builder, Action<HostBuilderContext, DiscordConfiguration> configureOptions)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                var config = new DiscordConfiguration();
+                configureOptions(context, config);
+
+                var client = new DiscordClient(config);
+
+                services.AddSingleton(config);
+                services.AddSingleton(client);
+            });
+
+            return builder;
+        }
+    }
+}
